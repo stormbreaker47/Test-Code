9b482b2ef29dc4b7664343c754f7dee7134d521c
sathish@07 <sathishsakthi1998@gmail.com>
2024-04-10 14:54:23 +0530

    first commit

--- Task/Task 5.js
+++ Task/Task 5.js
@@ -1,4 +1,20 @@
 
+// common use 
+
+< !DOCTYPE html >
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script src="Task 5.js"></script>
+</body>
+            </html>
+            
+
+
 // Get all the countries from Asia continent /region using Filter function
 // a.
 
